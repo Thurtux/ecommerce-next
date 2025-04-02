bd3b7284a9200e896a1844558b78a623600e9e45
Thurtux <arthursantoslopes91@gmail.com>
2025-04-02 14:05:47 -0300

    Adicionando Componente product

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import { ProductType } from "@/lib/ProductType";
-  
+import Product from "./components/Product";  
   async function getProducts() {
-    const res = await fetch("https://api.escuelajs.co/api/v1/products");
+    const res = await fetch("https://fakestoreapi.com/products");
     
-    if (!res.ok) {
+    if (!res.ok) {  
       throw new Error("Failed to fetch data");
     }
     return res.json();
@@ -16,7 +16,7 @@ import { ProductType } from "@/lib/ProductType";
     <div className="max-w-7xl mx-auto pt-8 px-8 xl:px-0">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gab-6">
       {products.map((product: ProductType) => (
-        <div key={product.id}>{product.title}</div>
+        <Product key={product.id} product={product} />
       ))}
       </div>
     </div>
